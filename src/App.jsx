f6276594c9244a60c7fdfa417a9666576c52aa62
Simpush-43 <tpsingh02101966@gmail.com>
2025-03-13 13:10:47 +0530

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,36 @@
+import { useState } from 'react'
+import './App.css'
+import './futureweather.css'
+import {useEffect} from 'react'
+import Card from '../../Componets/Card'
+import Input from '../../Componets/Input'
+import Button from '../../Componets/Button'
+import {useWeather } from '../../Context/Weather'
+import RefreshButton from '../../Componets/Refreshbutton'
+import FutureWeather from '../../Componets/FutureWeather'
+function App() {
+  const weather = useWeather();
+  console.log(weather)
+useEffect(() => {
+  // get current location
+  weather.fetchCurrentUserLocation()
+}, [])
+
+  
+  return (
+    <>
+      <div className='App'>
+        Search the weather
+      </div>
+      <hr />
+      <div className='Searchbox'>
+      <Input/><Button onClick={weather.fetchData} value='Search'/>
+      </div>
+      <Card/>
+      <RefreshButton value='Refresh' onClick={weather.fetchCurrentUserLocation}/>
+<FutureWeather/>
+    </>
+  )
+}
+
+export default App
